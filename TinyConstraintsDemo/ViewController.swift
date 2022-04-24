bf75502ff87f781eee9cbd72b7ddd368c5ae8a72
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-04-24 15:38:12 +0300

    Create views

--- TinyConstraintsDemo/ViewController.swift
+++ TinyConstraintsDemo/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import TinyConstraints
 
 class ViewController: UIViewController {
 
+    let tinyView0 = TinyView(backgroundColor: .link, cornerRadius: 0, borderWidth: 0)
+    let tinyView1 = TinyView(backgroundColor: .red)
+    let tinyView2 = TinyView(backgroundColor: .green)
+    let tinyView3 = TinyView(backgroundColor: .yellow)
+    let tinyView4 = TinyView(backgroundColor: .cyan)
+    let tinyView5 = TinyView(backgroundColor: .orange)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .lightGray
+
+        addViews()
+        addConstraints()
     }
-
-
 }
 
+
+extension ViewController {
+
+    private func addViews() {
+        view.addSubview(tinyView0)
+    }
+
+    private func addConstraints() {
+        tinyView0.edgesToSuperview()
+    }
+}
