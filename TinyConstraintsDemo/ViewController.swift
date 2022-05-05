ce119d3f1bdf960a0841778d6dfb7223a0a5451e
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-05-05 23:59:49 +0300

    Configure

--- TinyConstraintsDemo/ViewController.swift
+++ TinyConstraintsDemo/ViewController.swift
@@ -12,8 +12,9 @@ class ViewController: UIViewController {
 
     // MARK: - View
     let navBar = TinyView(backgroundColor: .lightGray, cornerRadius: 0, borderWidth: 0)
-    let borderBar = TinyView(backgroundColor: .gray, cornerRadius: 0, borderWidth: 0)
-    let secondBorderBar = TinyView(backgroundColor: .gray, cornerRadius: 0, borderWidth: 0)
+    let firstLine = TinyView(backgroundColor: .gray, cornerRadius: 0, borderWidth: 0)
+    let secondLine = TinyView(backgroundColor: .gray, cornerRadius: 0, borderWidth: 0)
+    let thirdLine = TinyView(backgroundColor: .gray, cornerRadius: 0, borderWidth: 0)
 
     lazy var timeBarButtonItem: UIImageView = {
         let view = UIImageView()
@@ -46,9 +47,9 @@ class ViewController: UIViewController {
         return view
     }()
 
-    lazy var containerView: UIView = {
+    lazy var businessInformationView: UIView = {
         let view = UIView()
-        view.backgroundColor = .lightGray
+        view.backgroundColor = .white
         return view
     }()
 
@@ -60,74 +61,92 @@ class ViewController: UIViewController {
 
         addViews()
         addConstraints()
+
+        let lines = [firstLine, secondLine, thirdLine]
+        configureLines(lines)
     }
 }
 
-// MARK: - 
+// MARK: -
 extension ViewController {
     private func addViews() {
         view.addSubview(navBar)
-        view.addSubview(borderBar)
+        view.addSubview(firstLine)
         view.addSubview(timeBarButtonItem)
         view.addSubview(menuBarButtonItem)
         view.addSubview(textDescription)
-        view.addSubview(secondBorderBar)
-        view.addSubview(containerView)
+        view.addSubview(secondLine)
+        view.addSubview(businessInformationView)
+        view.addSubview(thirdLine)
     }
 
     //MARK: - Constraints
     private func addConstraints() {
-
-        constrainsNavBar()
-        constrainsBorderBar()
-        constrainsTimeBarButtonItem()
-        constrainsMenuBarButtonItem()
-        constrainsTextDescription()
-        constrainsSecondBorderBar()
-        constrainsContainerView()
+        addNavBar()
+        addFirstLine()
+        addTimeBarButtonItem()
+        addMenuBarButtonItem()
+        addTextDescription()
+        addSecondLine()
+        addBusinessInformationView()
+        addThirdLine()
     }
 
-    private func constrainsNavBar() {
+    private func addNavBar() {
         navBar.height(100)
         navBar.alpha = 0.5
         navBar.edgesToSuperview(excluding: .bottom)
     }
 
-    private func constrainsBorderBar() {
-        borderBar.height(1)
-        borderBar.leadingToSuperview()
-        borderBar.trailingToSuperview()
-        borderBar.topToBottom(of: navBar)
+    private func addFirstLine() {
+        firstLine.leadingToSuperview()
+        firstLine.trailingToSuperview()
+        firstLine.topToBottom(of: navBar)
     }
 
-    private func constrainsTimeBarButtonItem() {
+    private func addTimeBarButtonItem() {
         timeBarButtonItem.leadingToSuperview(offset: 8)
         timeBarButtonItem.bottom(to: navBar, offset: -8)
         timeBarButtonItem.size(CGSize(width: 45, height: 45))
     }
 
-    private func constrainsMenuBarButtonItem() {
+    private func addMenuBarButtonItem() {
         menuBarButtonItem.trailingToSuperview(offset: 8)
         menuBarButtonItem.bottom(to: navBar, offset: -8)
         menuBarButtonItem.size(CGSize(width: 45, height: 45))
     }
 
-    private func constrainsTextDescription() {
+    private func addTextDescription() {
         textDescription.height(40)
         textDescription.leadingToSuperview()
         textDescription.trailingToSuperview()
-        textDescription.topToBottom(of: borderBar)
+        textDescription.topToBottom(of: firstLine)
     }
 
-    private func constrainsSecondBorderBar() {
-        secondBorderBar.height(1)
-        secondBorderBar.leadingToSuperview()
-        secondBorderBar.trailingToSuperview()
-        secondBorderBar.topToBottom(of: textDescription)
+    private func addSecondLine() {
+        secondLine.leadingToSuperview()
+        secondLine.trailingToSuperview()
+        secondLine.topToBottom(of: textDescription)
     }
 
-    private func constrainsContainerView() {
-        containerView.topToBottom(of: secondBorderBar)
-        containerView.edgesToSuperview(excluding: .top)
+    private func addBusinessInformationView() {
+        businessInformationView.height(400)
+        businessInformationView.topToBottom(of: secondLine)
+        businessInformationView.leadingToSuperview()
+        businessInformationView.trailingToSuperview()
+    }
+
+    private func addThirdLine() {
+        thirdLine.leadingToSuperview()
+        thirdLine.trailingToSuperview()
+        thirdLine.topToBottom(of: businessInformationView)
+    }
+
+    // MARK: - Configure
+    private func configureLines(_ borders: [TinyView]) {
+        for border in borders {
+            border.alpha = 0.3
+            border.height(1)
+        }
     }
 }
